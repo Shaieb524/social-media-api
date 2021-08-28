09426cbd53ba21aba3af7ec748e66f28af4a8d82
Parthkhandelwal0 <kparth2010@gmail.com>
2021-08-28 20:14:54 +0530

    initialized database

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRoute = require('./routes/users')
+
+dotenv.config();
+const start = async () => {
+    
+    if (!process.env.MONGO_URL) {
+        throw new Error('auth MONGO_URL must be defined');
+    }
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false, 
+        });
+        console.log('Server connected to MongoDb !');
+    } catch (err) {
+        console.error(err);
+    }
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.get("/health-check", (req,res)=>{
+    res.json("success")
+})
+
+app.use('/users',userRoute)
+
+app.listen(3000, () => {
+    console.log('Server is listening on 3000')
+});
+};
+start();
