a353d513804707bb0056fa8e9f7c354dfcb7ff45
Parthkhandelwal0 <kparth2010@gmail.com>
2021-09-04 01:59:26 +0530

    initialized database

--- index.js
+++ index.js
@@ -34,8 +34,12 @@ app.get("/", (req,res)=>{
 
 app.use('/users',userRoute)
 
-app.listen(8080, () => {
-    console.log('Server is listening on 8080')
-});
+// app.listen(8080, () => {
+//     console.log('Server is listening on 8080')
+// });
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+  
 };
 start();
