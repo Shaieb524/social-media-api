01902fc33f474a2f7fb2bbdcd2eda3bb299a4219
Parthkhandelwal0 <kparth2010@gmail.com>
2021-09-16 03:28:39 +0530

    added walletAddress

--- models/Users.js
+++ models/Users.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
         unique: true,
     },
     isWalletConnected: {
-        type: bool,
+        type: Boolean,
         default: false,
     },
     walletAddress :{
