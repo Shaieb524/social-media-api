df61e1df103dc03a41a27a400ff0a43d69cbd142
Joud Shaieb <joud.shaieb@medicus.ai>
2022-02-17 10:49:19 +0200

    get tags by tokenId

--- src/controllers/nft.ts
+++ src/controllers/nft.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from "express";
 import MainController from './main'
 import { NftServices } from '../services/nft'
+import ErrorValidator from '../utils/error-validator'
+import GeneralHelper from '../utils/general-helper'
+import { NFTModel } from '../models/nft'
+
 
 class NftController extends MainController {
     
@@ -7,6 +12,25 @@ class NftController extends MainController {
         super(nftService)
         this.setRoutes()
     }
+
+    setRoutes() {
+        super.setRoutes()
+        this.router.route("/get-token-tags").post(this.getTokenTags);
+    }
+
+    private getTokenTags = async (req: Request, res: Response) => {
+        try {
+            const token = await NFTModel.findOne({tokenId: req.body.tokenId})
+            if (!token) {
+                res.status(ErrorValidator.NOT_FOUND).send(ErrorValidator.notFound("Token was not found!"))
+            } else {
+                res.status(ErrorValidator.SUCCESS).send(token)
+            }
+        } catch (e) {
+            GeneralHelper.checkTryErrorTypeAndResponse(e, 'creating newsfeed post', res)
+        }
+    }
+
 }
 
 export default NftController
