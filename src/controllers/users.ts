a90a9ea2bdb51b1b771c74ecd362bb8048868385
Joud Shaieb <joud.shaieb@medicus.ai>
2022-02-18 18:37:09 +0200

    restore comments

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -19,9 +19,9 @@ class UsersController extends MainController {
         super.setRoutes()
         this.router.route("/:username").get(this.findUserByName);
         this.router.route("/search").post(this.searchUsers);
-        // this.router.route("/walletConnect/:ref?").post(this.walletConnect);
-        // this.router.route("/NFTtag").post(this.tagUserNft);
-        // this.router.route("/NFTsearch/:tag").get(this.searchNftByTag);
+        this.router.route("/walletConnect/:ref?").post(this.walletConnect);
+        this.router.route("/NFTtag").post(this.tagUserNft);
+        this.router.route("/NFTsearch/:tag").get(this.searchNftByTag);
         this.router.route("/TaggedNFTs/:owner").get(this.getTaggedNftsForUser);
         this.router.route("/follow").post(this.followUser);
         this.router.route("/unfollow").post(this.unfollowUser);
@@ -49,80 +49,80 @@ class UsersController extends MainController {
         }
     }
 
-    // private walletConnect = async (req: Request, res: Response) => {
-    //     try {
+    private walletConnect = async (req: Request, res: Response) => {
+        try {
 
-    //         if (req.query.ref) {
-    //             const ref = req.query.ref;
-    //             const rUser = await UsersModel.findOne({ username: ref });
-    //             if (rUser) {
-    //                 rUser.referralCount = rUser.referralCount + 1;
-    //                 await rUser.save();
-    //             } else {
-    //                 res.status(ErrorValidator.NOT_FOUND).send(ErrorValidator.notFound("incorrect referral link"));
-    //             }
-    //         }
+            if (req.query.ref) {
+                const ref = req.query.ref;
+                const rUser = await UsersModel.findOne({ username: ref });
+                if (rUser) {
+                    rUser.referralCount = rUser.referralCount + 1;
+                    await rUser.save();
+                } else {
+                    res.status(ErrorValidator.NOT_FOUND).send(ErrorValidator.notFound("incorrect referral link"));
+                }
+            }
 
-    //         const existedUser = await UsersModel.findOne({ walletAddress: req.body.walletAddress });
-    //         if (!existedUser) {
-    //             const assignedUsername = superheroes.random();
-    //             const createdUserData = {
-    //                 walletAddress: req.body.walletAddress,
-    //                 isWalletConnected: true,
-    //                 username: assignedUsername.replace(/\s/g, ""),
-    //             }
+            const existedUser = await UsersModel.findOne({ walletAddress: req.body.walletAddress });
+            if (!existedUser) {
+                const assignedUsername = superheroes.random();
+                const createdUserData = {
+                    walletAddress: req.body.walletAddress,
+                    isWalletConnected: true,
+                    username: assignedUsername.replace(/\s/g, ""),
+                }
 
-    //             const addedUser = await this.usersService.addItem(createdUserData)
-    //             res.status(ErrorValidator.SUCCESS).json(addedUser);
-    //         } else {
-    //             res.status(ErrorValidator.SUCCESS).json({message : 'User already exists!'});
-    //         }
-    //     } catch (e) {
-    //         GeneralHelper.checkTryErrorTypeAndResponse(e, 'connecting wallet', res)
-    //     }
-    // }
+                const addedUser = await this.usersService.addItem(createdUserData)
+                res.status(ErrorValidator.SUCCESS).json(addedUser);
+            } else {
+                res.status(ErrorValidator.SUCCESS).json({message : 'User already exists!'});
+            }
+        } catch (e) {
+            GeneralHelper.checkTryErrorTypeAndResponse(e, 'connecting wallet', res)
+        }
+    }
 
-    // private tagUserNft = async (req: Request, res: Response) => {
-    //     try {
-    //         const user = await UsersModel.findOne({walletAddress: req.body.owner})
-    //         if (!user) {
-    //             res.status(ErrorValidator.NOT_FOUND).send(ErrorValidator.notFound("User does not exist!"))
-    //         }
-    //         else {
-    //             let nftService = new NftServices(NFTModel)
-    //             let {owner, tokenId, address, tags} = req.body
+    private tagUserNft = async (req: Request, res: Response) => {
+        try {
+            const user = await UsersModel.findOne({walletAddress: req.body.owner})
+            if (!user) {
+                res.status(ErrorValidator.NOT_FOUND).send(ErrorValidator.notFound("User does not exist!"))
+            }
+            else {
+                let nftService = new NftServices(NFTModel)
+                let {owner, tokenId, address, tags} = req.body
 
-    //             // creaate nft
-    //             await nftService.addItem({
-    //                 owner: owner,
-    //                 tokenId: tokenId,
-    //                 address: address,
-    //                 tags: tags
-    //             })    
+                // creaate nft
+                await nftService.addItem({
+                    owner: owner,
+                    tokenId: tokenId,
+                    address: address,
+                    tags: tags
+                })    
 
-    //             // update user's nfts
-    //             const uNFT = await NFTModel.find({ owner: req.body.owner })
-    //             await user.updateOne({NFTs: uNFT})
+                // update user's nfts
+                const uNFT = await NFTModel.find({ owner: req.body.owner })
+                await user.updateOne({NFTs: uNFT})
 
-    //             res.status(ErrorValidator.SUCCESS).send(ErrorValidator.success('Nft added successfully!'))
-    //         }
-    //     } catch (e) {
-    //         GeneralHelper.checkTryErrorTypeAndResponse(e, 'tagging user NFT', res)
-    //     }
-    // }
+                res.status(ErrorValidator.SUCCESS).send(ErrorValidator.success('Nft added successfully!'))
+            }
+        } catch (e) {
+            GeneralHelper.checkTryErrorTypeAndResponse(e, 'tagging user NFT', res)
+        }
+    }
 
-    // private searchNftByTag = async (req: Request, res: Response) => {
-    //     if (req.params.tag) {
-    //         try {
-    //             const nfts = await NFTModel.find({tags: {"$in": [req.params.tag]}}) 
-    //             if (nfts) res.status(ErrorValidator.SUCCESS).json(nfts)
-    //         } catch (e) {
-    //             GeneralHelper.checkTryErrorTypeAndResponse(e, 'getting NFT tag', res)
-    //         }
-    //     } else {
-    //         res.status(ErrorValidator.BAD_REQUEST).send(ErrorValidator.badRequest("no nft tag in request"));
-    //     }
-    // } 
+    private searchNftByTag = async (req: Request, res: Response) => {
+        if (req.params.tag) {
+            try {
+                const nfts = await NFTModel.find({tags: {"$in": [req.params.tag]}}) 
+                if (nfts) res.status(ErrorValidator.SUCCESS).json(nfts)
+            } catch (e) {
+                GeneralHelper.checkTryErrorTypeAndResponse(e, 'getting NFT tag', res)
+            }
+        } else {
+            res.status(ErrorValidator.BAD_REQUEST).send(ErrorValidator.badRequest("no nft tag in request"));
+        }
+    } 
 
     private getTaggedNftsForUser = async (req: Request, res: Response) => {
         try {
