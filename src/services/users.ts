034f6ea9824355fa7f606eaa6ee005b4d4dbfadd
Joud <joud.shaieb@medicus.ai>
2022-02-18 18:25:02 +0200

    add follow endpoint

--- src/services/users.ts
+++ src/services/users.ts
@@ -4,4 +4,20 @@ export class UsersServices extends MainServices {
     constructor(usersModel : any) {
         super(usersModel)
     }
+
+    public async followUser(callerId: string, targetId: string) {
+        let callerUser:any = await this.serivceModel.findById({_id: callerId})
+        let targetUser:any = await this.serivceModel.findById({_id: targetId})
+
+        if (!callerUser || !targetUser) {
+            return 400
+        } else {
+        	callerUser.followings.push(targetUser.username)
+        	targetUser.followers.push(callerUser.username)  
+            this.updateItem(callerId, callerUser)
+            this.updateItem(targetId, targetUser)
+            return 200
+        }
+    }
+
 }
